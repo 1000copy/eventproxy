574ef2e9f1895c833e555f1b177416a684b619f6
liu chuan jun <1000copy@gmail.com>
2015-06-03 10:54:09 +0800

    simplized

--- test/cia.js
+++ test/cia.js
@@ -10,84 +10,34 @@ EventProxy.prototype.bind = function (ev, callback) {
   this._callbacks[ev].push(callback);
   return this;
 };
-EventProxy.prototype.clear = function (eventname) {
-  var calls = this._callbacks;
-  if (!eventname) {      
-    this._callbacks = {};
-  }     
+EventProxy.prototype.clear = function () {
+  this._callbacks = {};       
   return this;
 };  
-/**
- * Trigger an event, firing all bound callbacks. Callbacks are passed the
- * same arguments as `trigger` is, apart from the event name.
- * Listening for `"all"` passes the true event name as the first argument.
- * @param {String} eventname Event name
- * @param {Mix} data Pass in data
- */
+
 EventProxy.prototype.fire = function (eventname, data) {
-  var list, ev, callback, i, l;
-  var both = 2;
-  var calls = this._callbacks;
-    // ev = both ? eventname : ALL_EVENT;
-    ev = eventname 
-    list = calls[ev];
-    // console.log(list)
-    if (list) {
-      for (i = 0, l = list.length; i < l; i++) {
-        if (!(callback = list[i])) {
-          list.splice(i, 1);
-          i--;
-          l--;
-        } else {
-          var args = [];
-          var start = both ? 1 : 0;
-          for (var j = start; j < arguments.length; j++) {
-            args.push(arguments[j]);
-          }
-          callback.apply(this, args);
-        }
-      }    
+  var list = this._callbacks[eventname];    
+  if (list) {
+    for (var i = 0;i < list.length; i++) {              
+        var args = SLICE.call(arguments, 1);
+        list[i].apply(this, args);
+    }    
   }
   return this;
 };
 
-EventProxy.prototype.done = function (handler, callback) {
+EventProxy.prototype.done = function (handler) {
   var that = this;
   return function (err, data) {
     if (err) {
-      // put all arguments to the error handler
+      // fire error event and put all arguments to the error handler
       return that.fire.apply(that, ['error'].concat(SLICE.call(arguments)));
     }
-
-    // callback(err, args1, args2, ...)
-    var args = SLICE.call(arguments, 1);
-
-    if (typeof handler === 'string') {
-      // getAsync(query, ep.done('query'));
-      // or
-      // getAsync(query, ep.done('query', function (data) {
-      //   return data.trim();
-      // }));
-      if (callback) {
-        // only replace the args when it really return a result
-        return that.fire(handler, callback.apply(null, args));
-      } else {
-        // put all arguments to the done handler
-        //ep.done('some');
-        //ep.on('some', function(args1, args2, ...){});
-        // fire(handle,arg1,arg2,arg3)
-        return that.fire.apply(that, [handler].concat(args));
-      }
-    }
-
-    // speed improve for mostly case: `callback(err, data)`
-    if (arguments.length <= 2) {
-      return handler(data);
-    }
-
-    // callback(err, args1, args2, ...)
-    handler.apply(null, args);
+    // callback(err, args1, args2, ...) exclude err arg
+    var args = SLICE.call(arguments, 1);    
+    // put all arguments to the done handler
+    //ep.done('some');    
+    return that.fire.apply(that, [handler].concat(args));
   };
 };
-
 module.exports.EventProxy = EventProxy
--- test/test1.js
+++ test/test1.js
@@ -3,7 +3,7 @@ var should = require('chai').should();
 var expect = require('chai').expect;
 var pedding = require('pedding');
 var EventProxy = require('./cia.js').EventProxy;
-console.log(EventProxy)
+
 var __filename = 'mock_file.txt';
 var __content = 'Foo bar baz'
 var fs = {
@@ -31,26 +31,7 @@ function step3(callback){
     callback() 
 }
 describe("cia", function () {
-  it('done(event)', function (done) {
-    var ep = new EventProxy 
-    ep.bind('event1', function (data) {
-      should.exist(data);
-      expect(data).to.equal(__content)
-      done();
-    });
-    ep.bind('error', done);
-    fs.readFile(__filename, ep.done('event1'));
-  });
-  it('loaded', function (done) {
-    var ep = new EventProxy 
-    ep.bind('loaded', function (data) {
-      should.exist(data);
-      expect(data).to.equal(__content)
-      done();
-    });
-    ep.bind('error', done);
-    fs.readFile(__filename, ep.done('loaded'));
-  });
+  
   it('step', function (done) {
     step1(function(){
       step2(function(){
@@ -72,5 +53,69 @@ describe("cia", function () {
       done()
     });
     step1(ep.done("s1"))
+  });
+  it('fooargs', function (done) {
+    // 函数参数的声明真的是不重要的，不过是给用户的语法糖
+    var foo = function(err ,data){
+      var args = Array.prototype.slice.call(arguments, 1);
+      expect(args).to.deep.equal([1,2,3])
+    }
+    var foo1 = function(){
+      var args = Array.prototype.slice.call(arguments, 1);
+      expect(args).to.deep.equal([1,2,3])
+    }
+    foo("error",1,2,3)
+    foo1("error",1,2,3)
+    done()    
+  });
+  it('errorargs', function (done) {
+      var fs1 = {
+        readFile: function (filename, encode, callback) {
+          if (typeof encode === 'function') {
+            callback = encode;
+            encode = null;
+          }
+          setTimeout(function () {
+            if (filename === 'not exist file') {              
+              return callback(new Error('ENOENT, open \'not exist file\''),1,2,3);
+            }
+            callback(null, __content);
+          }, 10);
+        }
+      };
+    var ep = new EventProxy ;
+    ep.bind("error",function(){
+      expect(Array.prototype.slice.call(arguments,1)).to.deep.equal([1,2,3])
+      done()    
+    })
+    fs1.readFile("not exist file",ep.done("e1"))
+    
+  });
+    it('errorargs', function (done) {
+    var ep = new EventProxy ;
+    ep.bind("e1",function(data){
+      expect(data).to.deep.equal(__content)
+      done()    
+    })
+    fs.readFile(__filename,ep.done("e1"))    
+  });
+  it('arrayslice', function (done) {
+    function foo(a,b,c){
+      expect(Array.prototype.slice.call(arguments,1)).to.deep.equal([2,3])
+      expect(Array.prototype.slice     (arguments,1)).to.deep.equal([])
+      done();
+    }
+    foo(1,2,3)
+  });
+   it('foo2', function (done) {
+    function foo(a,b,c){
+      // 不会调用到此
+      expect(0).to.equal(1)      
+    }
+    function foo(a,b){
+      expect(arguments.length).to.equal(3)
+      done();
+    }
+    foo(1,2,3)    
   });
 })
